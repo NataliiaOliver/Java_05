bcc2ef6d329f93654a814afc783eae372a5707bc
Nataliia Oliver <psvnatali@gmail.com>
2022-10-19 23:57:58 -0500

    Method of creating an array with Random int numbers

--- src/main/java/project_utils/Utils.java
+++ src/main/java/project_utils/Utils.java
@@ -3,7 +3,7 @@ package project_utils;
 public class Utils {
 
     /**
-     * метод генераци Рэндомных чисел (Random Numbers):
+     * Random number generation methods:
      */
     public static int getRandomInt(int upper, int lower) {
 
@@ -13,5 +13,17 @@ public class Utils {
     public static double getRandomDouble(int upper, int lower) {
 
         return ((Math.random() * (upper - lower) + lower));
+    }
+
+    /**
+     * Method of creating an array with Random int numbers:
+     */
+    public static int[] createArrayRandomInt(int length, int upper, int lower) {
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getRandomInt(upper, lower);
+        }
+
+        return array;
     }
 }
