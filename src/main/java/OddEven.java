fea1425d71008cada97f2dd2d99e0f0c43ab6e02
Nataliia Oliver <psvnatali@gmail.com>
2022-10-21 09:06:12 -0500

    created OddEven algorithm and tests

---
+++ src/main/java/OddEven.java
@@ -0,0 +1,20 @@
+public class OddEven {
+
+    public String OddEven(long number) {
+        String even = "Even";
+        String odd = "Odd";
+        String undefined = "Undefined";
+
+        if (number <= Integer.MAX_VALUE && number >= Integer.MIN_VALUE) {
+            if (number % 2 == 0) {
+
+                return even;
+            } else {
+
+                return odd;
+            }
+        }
+
+        return undefined;
+    }
+}
---
+++ src/test/java/OddEvenTest.java
@@ -0,0 +1,60 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OddEvenTest {
+
+    @Test(priority = 3)
+    public void testZeroNumber_HappyPath() {
+
+        int number = 0;
+        String expectedResult = "Even";
+
+        String actualResult = new OddEven().OddEven(number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(priority = 1)
+    public void testEvenNumber_HappyPath() {
+
+        int number = 222222;
+        String expectedResult = "Even";
+
+        String actualResult = new OddEven().OddEven(number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(priority = 2)
+    public void testOddNumber_HappyPath() {
+
+        int number = 345;
+        String expectedResult = "Odd";
+
+        String actualResult = new OddEven().OddEven(number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(priority = 4)
+    public void testGreaterThanIntegerMax_Negative() {
+
+        long number = Integer.MAX_VALUE + 1L;
+        String expectedResult = "Undefined";
+
+        String actualResult = new OddEven().OddEven(number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test(priority = 5)
+    public void testLessThanIntegerMin_Negative() {
+
+        long number = Integer.MIN_VALUE - 1L;
+        String expectedResult = "Undefined";
+
+        String actualResult = new OddEven().OddEven(number);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
