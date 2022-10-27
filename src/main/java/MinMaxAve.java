df8f08b57b9f4873bac9b7992307aac82a92f49a
Nataliia Oliver <psvnatali@gmail.com>
2022-10-26 21:12:44 -0500

    created CreateArray and CreateArrayTest, codes and tests added

--- src/main/java/MinMaxAve.java
+++ src/main/java/MinMaxAve.java
@@ -1,6 +1,14 @@
+import project_utils.Utils;
+
 public class MinMaxAve {
 
-//    public int[] minMaxAveAlgorithm(int[] array, int a, int b) {
-//
-//    }
+    public int[] minMaxAveAlgorithm(int[] array, int index1, int index2) {
+        if (Utils.checkForNullArray(array) && Utils.checkForEmptyArray(array)) {
+
+        }
+
+        return new int[]{};
+    }
 }
+
+
